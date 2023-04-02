cbf23f6fa96d53c33b70f388d05631d6ec3b084e
gelu@codeindustry.net <gelu@codeindustry.net>
2023-04-02 09:19:07 +0300

    add product entity

---
+++ ShoppingCart/ShoppinCart.DAL/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using ShoppinCart.DAL.Entities;
+
+namespace ShoppinCart.DAL
+{
+    public class AppDbContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+
+        public AppDbContext(IConfiguration configuration) => _configuration = configuration;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(_configuration.GetConnectionString("sqlConnection"));
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
--- ShoppingCart/ShoppingCart/Program.cs
+++ ShoppingCart/ShoppingCart/Program.cs
@@ -1,8 +1,12 @@
+using ShoppinCart.DAL;
+using System;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>();
 
 var app = builder.Build();
 

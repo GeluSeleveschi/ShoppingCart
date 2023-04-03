534c3b466b53755305a77abf812dfb486c733d8f
gelu@codeindustry.net <gelu@codeindustry.net>
2023-04-03 06:21:13 +0300

    add product service

--- ShoppingCart/ShoppingCart/ClientApp/src/app/product.service.ts
+++ ShoppingCart/ShoppingCart/ClientApp/src/app/product.service.ts
@@ -6,10 +6,10 @@ import { environment } from '../environments/environment';
   providedIn: 'root'
 })
 export class ProductService {
-  URL: string = `${environment.serverUrl}/product`;
+  URL: string = `${environment.serverUrl}/api/product`;
   constructor(private http: HttpClient) { }
 
   addProduct(product: any) {
-    this.http.post(this.URL, product);
+    return this.http.post(this.URL, product);
   }
 }
